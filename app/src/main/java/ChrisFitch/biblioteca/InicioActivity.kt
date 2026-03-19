d67f273a71c8d05b789c05936b48540d572bae37
ArellanoAndre <isaac.arellano229842@potros.itson.edu.mx>
2026-03-19 15:44:35 -0700

    loggin

---
+++ app/src/main/java/ChrisFitch/biblioteca/InicioActivity.kt
@@ -0,0 +1,24 @@
+package ChrisFitch.biblioteca
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InicioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+
+        btnEntrar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
