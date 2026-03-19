5eaa469567a46f686cf31c3f66fb5c07180daffd
ArellanoAndre <isaac.arellano229842@potros.itson.edu.mx>
2026-03-19 15:16:27 -0700

    Lista de libros

---
+++ app/src/main/java/components/Book.kt
@@ -0,0 +1,12 @@
+package components
+
+data class Book(
+    val title: String,
+    val id: Int,
+    val author: String,
+    val genre: String,
+    val description: String,
+    val pages: Int,
+    val available: Boolean,
+    val image: Int
+)
---
+++ app/src/main/java/components/BooksDummy.kt
@@ -0,0 +1,65 @@
+package components
+
+import ChrisFitch.biblioteca.R
+
+class BooksDummy {
+    val bookList = listOf(
+
+        Book(
+            title = "Cien años de soledad",
+            id = 1,
+            author = "Gabriel García Márquez",
+            genre = "Realismo mágico",
+            description = "Historia de la familia Buendía en Macondo.",
+            pages = 417,
+            available = true,
+            image = R.drawable.cien
+        ),
+
+        Book(
+            title = "Don Quijote de la Mancha",
+            id = 2,
+            author = "Miguel de Cervantes",
+            genre = "Novela clásica",
+            description = "Aventuras de un caballero que lucha contra molinos.",
+            pages = 863,
+            available = false,
+            image = R.drawable.donquijote
+        ),
+
+        Book(
+            title = "El Principito",
+            id = 3,
+            author = "Antoine de Saint-Exupéry",
+            genre = "Fábula",
+            description = "Historia sobre la amistad, el amor y la vida.",
+            pages = 96,
+            available = true,
+            image = R.drawable.principito
+        ),
+
+        Book(
+            title = "Harry Potter y la piedra filosofal",
+            id = 5,
+            author = "J.K. Rowling",
+            genre = "Fantasía",
+            description = "Un niño descubre que es mago.",
+            pages = 309,
+            available = true,
+            image = R.drawable.harrypoter
+        )
+
+    )
+
+    fun showAllBooks(): List<Book> {
+        return bookList
+    }
+
+    fun getOneBook(): Book {
+        return bookList.random()
+    }
+
+    fun getBook(id: Int): Book {
+        return bookList.first { it.id == id }
+    }
+}
