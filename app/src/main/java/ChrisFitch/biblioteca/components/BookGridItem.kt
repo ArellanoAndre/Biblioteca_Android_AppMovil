8dfc9996bc63295b724bde84e976fbb524da8736
GitChrisLopez <sabine.fitch252379@potros.itson.edu.mx>
2026-03-19 15:51:58 -0700

    book update

---
+++ app/src/main/java/ChrisFitch/biblioteca/components/BookGridItem.kt
@@ -0,0 +1,67 @@
+package ChrisFitch.biblioteca.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun BookGridItem(book: Book, onNavigationDetail: (id: Int) -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.clickable(onClick = { onNavigationDetail(book.id) })
+    ) {
+        Box {
+            Image(
+                painter = painterResource(book.image),
+                contentDescription = book.title,
+                modifier = Modifier.size(150.dp),
+                contentScale = ContentScale.Fit
+            )
+            Row(
+                Modifier
+                    .size(35.dp, 35.dp)
+                    .background(Color.DarkGray, shape = RoundedCornerShape(40.dp))
+                    .align(Alignment.TopEnd),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "${book.id}",
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 9.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BookItemPreview() {
+    val dummy = BooksDummy()
+    BookGridItem(
+        book = dummy.getOneBook(),
+        onNavigationDetail = { id -> }
+    )
+}
